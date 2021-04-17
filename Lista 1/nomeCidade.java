82042f6a036a722747a367a26d2b7a431842bcef
Leandro Damasceno Alves <leandro.d.alves@outlook.com>
2021-04-17 03:33:56 -0300

    primeiro código.

---
+++ Lista 1/nomeCidade.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+/*Faça um programa que o usuário digita o nome e a cidade onde mora e logo em seguida o 
+programa responde com a mensagem “Olá bem vindo [nome]! Soube que você mora na cidade de [cidade] ”.
+
+@autor Leandro Damasceno Alves 
+
+*/
+
+public class nomeCidade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		JOptionPane.showMessageDialog(null,	"Olá Humano!\nBora conversar?");
+		
+		String nickname = JOptionPane.showInputDialog("Qual é o seu nome?");
+		
+		JOptionPane.showMessageDialog(null,	"\nQualé, chama isso de nome? haha! \npode me chamar de JV", nickname, JOptionPane.WARNING_MESSAGE);
+		
+		String guildacity = JOptionPane.showInputDialog("Mora em qual quebrada?");
+					
+		JOptionPane.showMessageDialog(null,	"Não botei fé nessa quebrada hein! \nVou confirmar com o seu IP....");
+		
+		JOptionPane.showMessageDialog(null,	"Olá, seja Bem vindo "+nickname+"!\nRastrei o seu IP, diz que sua localização é em "+guildacity);
+		
+		
+		System.exit(0);
+
+	}
+
+}
